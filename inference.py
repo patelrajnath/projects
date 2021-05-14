dce36bb4d3b4b11151ba2961d57958dd03c8d62a
Raj Nath Patel <patelrajnath@gmail.com>
2021-05-14 13:03:42 +0100

    Refactor the code to csv into iob

---
+++ inference.py
@@ -0,0 +1,7 @@
+import spacy
+
+nlp = spacy.load("models/snips/model-best")
+doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
